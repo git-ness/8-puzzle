54c44a465c9f3d05799b82bd1791e487397b1d4f
Elsa Hill <ehill@newrelic.com>
2017-11-24 17:54:55 -0800

    Created IndexOfCorrectValues inner class, added an ArrayList to store it, goal is to create an easy index lookup of a correctBoard to subtract indices for the manhattan quantities

--- src/Board.java
+++ src/Board.java
@@ -1,48 +1,57 @@
-import edu.princeton.cs.algs4.In;
-import edu.princeton.cs.algs4.StdOut;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Board {
 
     private int[][] blocks;
-    private int[][] intialBoardCopy;
+    private int[][] initialBoardCopy;
     private int[][] correctBoard;
+    private ArrayList<IndexOfValues> indexOfCorrectBoard;
     private int[][] twinboard;
 
     public Board(int[][] blocks) {          // construct a board from an n-by-n array of blocks
         // (where blocks[i][j] = block in row i, column j)
-        this.intialBoardCopy = new int[blocks.length][blocks.length];
+        this.initialBoardCopy = new int[blocks.length][blocks.length];
+        this.correctBoard = new int[blocks.length][blocks.length];
         this.blocks = blocks;
 
         // Deep copy of 2 dem array.
         for (int i = 0; i < blocks.length; i++) {
             for (int j = 0; j < blocks.length; j++) {
-                this.intialBoardCopy[i][j] = blocks[i][j];
+                this.initialBoardCopy[i][j] = blocks[i][j];
             }
         }
 
-//        for (int e = 0; e < blocks.length; e++) {
-//            for (int f = 0; f < blocks.length; f++) {
-//                int correctValues = 1;
-//                correctBoard[e][f] = correctValues;
-//                correctValues++;
-//            }
-//        }
+        int correctValues = 1;
+        this.indexOfCorrectBoard = new ArrayList<>();
+        for (int e = 0; e < blocks.length; e++) {
+            for (int f = 0; f < blocks.length; f++) {
+                correctBoard[e][f] = correctValues;
+                indexOfCorrectValues(correctValues, e, f);
+
+                correctValues++;
+            }
+        }
+        // Sets the last value to be the "blank" space, where 0 is used.
+        correctBoard[blocks.length - 1][blocks.length - 1] = 0;
+    }
+
+    private void indexOfCorrectValues(int correctValues, int e, int f) {
+        IndexOfValues indexOfValues = new IndexOfValues(correctValues, e, f);
+        this.indexOfCorrectBoard.add(indexOfValues);
     }
 
     public int dimension() {                // board dimension n
-        return intialBoardCopy.length;
+        return initialBoardCopy.length;
     }
 
     public int hamming() {                  // number of blocks out of place
         int hammingCount = 0;
         int correctValue = 0;
 
-        for (int i = 0; i < intialBoardCopy.length; i++) {
-            for (int j = 0; j < intialBoardCopy.length; j++) {
-                int rolColvalue = intialBoardCopy[i][j];
+        for (int i = 0; i < initialBoardCopy.length; i++) {
+            for (int j = 0; j < initialBoardCopy.length; j++) {
+                int rolColvalue = initialBoardCopy[i][j];
 
                 correctValue++;
                 if (rolColvalue == correctValue && rolColvalue == 0) {
@@ -57,19 +66,79 @@ public class Board {
 
     public int manhattan() {                // sum of Manhattan distances between blocks and goal
         int manhattanValue = 0;
-        for (int i = 0; i < intialBoardCopy.length; i++) {
-            for (int j = 0; j < intialBoardCopy.length; i++) {
-                int goal = 1;
-                int blockValue = intialBoardCopy[i][j];
+        int goal = 1;
 
-                if (blockValue != goal) {
-                    // Subtract indexes from the correct value and take the abs of that value.
+        int iCorrectIndex = 0;
+        int jCorrectIndex = 0;
+        int iDifference;
+        int jDifference;
 
+        IndexOfValues index = indexOfCorrectBoard.get(4);
+        int iIndex = index.getiIndex();
+        int jIndex = index.getjIndex();
+
+        System.out.println("iIndex = " + iIndex + " jIndex = " + jIndex);
+
+
+        for (int i = 0; i < initialBoardCopy.length; i++) {
+            for (int j = 0; j < initialBoardCopy.length; j++) {
+
+                // Grab the i and j location of the correct value to
+                // find the absolute difference for the Manhattan distance.
+
+                if (iCorrectIndex == initialBoardCopy.length) {
+                    iCorrectIndex = 0;
                 }
+
+                if (jCorrectIndex == initialBoardCopy.length) {
+                    jCorrectIndex = 0;
+                }
+
+                int blockValue = initialBoardCopy[i][j];
+
+                if (blockValue != goal && initialBoardCopy[i][j] != 0) {
+                    // To find the Manhattan distance, subtract indexes
+                    // from the correct i and j location values and take the abs of that value
+                    iDifference = Math.abs(iCorrectIndex);
+                    jDifference = Math.abs(jCorrectIndex);
+
+                    manhattanValue += iDifference;
+                    manhattanValue += jDifference;
+                }
+
                 goal++;
+                iCorrectIndex++;
+
             }
+            jCorrectIndex++;
         }
-        return manhattanValue;
+        return 0;
+    }
+
+    private class IndexOfValues {
+        private int actualValue;
+        private int iIndex;
+        private int jIndex;
+
+        public IndexOfValues(int actualValue, int iIndex, int jIndex) {
+            this.actualValue = actualValue;
+            this.iIndex = iIndex;
+            this.jIndex = jIndex;
+        }
+
+        public int getIndexOfValue(int actualValue) {
+            return actualValue;
+        }
+
+        public int getiIndex() {
+            return iIndex;
+        }
+
+        public int getjIndex() {
+            return jIndex;
+        }
+
+
     }
 
     public boolean isGoal() {               // is this board the goal board?
@@ -79,9 +148,9 @@ public class Board {
     public Board twin() {                   // a board that is obtained by exchanging any pair of blocks
 
         // Deep copy that will soon return the twin board.
-        for (int i = 0; i < intialBoardCopy.length; i++) {
-            for (int j = 0; j < intialBoardCopy.length; j++) {
-                this.twinboard[i][j] = intialBoardCopy[i][j];
+        for (int i = 0; i < initialBoardCopy.length; i++) {
+            for (int j = 0; j < initialBoardCopy.length; j++) {
+                this.twinboard[i][j] = initialBoardCopy[i][j];
             }
         }
 
@@ -118,24 +187,25 @@ public class Board {
     public Iterable<Board> neighbors() {    // all neighboring boards
 
         ArrayList<Board> listOfBoards = new ArrayList<>();
-        listOfBoards.add(new Board(intialBoardCopy));
+        listOfBoards.add(new Board(initialBoardCopy));
 
 //        if ()
 
         return null;
     }
 
-    public String toString() {              // string representation of this board (in the output format specified below)
-        int n = dimension();
+    public String toString() {
+        int n = initialBoardCopy.length;
 
-        StringBuilder str = new StringBuilder();
-        str.append(n + "\n");
+        StringBuilder s = new StringBuilder();
+//        s.append(n + "\n");
         for (int i = 0; i < n; i++) {
-            str.append(String.format("%2d ", intialBoardCopy));
+            for (int j = 0; j < n; j++) {
+                s.append(String.format("%2d ", correctBoard[i][j]));
+            }
+            s.append("\n");
         }
-        str.append("\n");
-
-        return str.toString();
+        return s.toString();
     }
 
     public static void main(String[] args) { // unit tests (not graded)
@@ -144,11 +214,12 @@ public class Board {
 
         int[][] realBoardComplete = {
 
-                {1, 2, 3},
-                {4, 5, 6},
-                {7, 8, 0}};
+                {1, 2, 7},
+                {4, 0, 6},
+                {3, 8, 5}}; // If it's 0 or doesn't match the goal, add the index to the arrayList.
 
         Board boardExample = new Board(realBoardComplete);
+        System.out.println(boardExample.manhattan());
 
 
     }
